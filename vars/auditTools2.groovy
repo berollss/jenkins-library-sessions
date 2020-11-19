fc8cf2392ce0012c14ee0291ee04c88838762f35
Berta Lazaro <berta.lazaro@roche.com>
2020-11-19 17:08:05 +0100

    added audittools2 script

---
+++ vars/auditTools2.groovy
@@ -0,0 +1,11 @@
+def call() { // necesario que sea así para que jenkins pueda llamar a auditTools 
+    node { // scripted pipelines, para poder ponerlo como un step en una pipeline
+        echo %{config.message}
+        sh '''
+            git version
+            docker version
+            dotnet --list-sdks
+            dotnet --list-runtimes
+        '''
+    }
+}
