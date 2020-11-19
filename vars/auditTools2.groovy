c640e891c953584e5b7e396fe64fb8f00237d1eb
Berta Lazaro <berta.lazaro@roche.com>
2020-11-19 17:08:51 +0100

    added Map

--- vars/auditTools2.groovy
+++ vars/auditTools2.groovy
@@ -1,4 +1,4 @@
-def call() { // necesario que sea así para que jenkins pueda llamar a auditTools 
+def call(Map config) { // necesario que sea así para que jenkins pueda llamar a auditTools 
     node { // scripted pipelines, para poder ponerlo como un step en una pipeline
         echo %{config.message}
         sh '''
